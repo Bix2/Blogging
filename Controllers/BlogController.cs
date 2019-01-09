b4ae4c1652786b53b23ee991d9a7434db906a594
Bix2 <bibi.treffers@outlook.com>
2019-01-09 18:49:42 +0100

    added validation fallback

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -23,39 +23,50 @@ namespace Blogging.Controllers
         [HttpGet()]
         public IActionResult Index()
         {
-            var posts =  _blogContext.Posts.ToList();
-            return View(posts);
+            var model = new PostListVM();
+            model.Posts = _blogContext.Posts.ToList();
+            return View(model);
         }  
 
         [Route("/create")]
         [HttpGet]
         public IActionResult Create()
         {
-            var authors = _blogContext.Authors
+          /*   var authors = _blogContext.Authors
                 .Select(x => new SelectListItem {
-                    Value = x.AuthorId.ToString(),
+                    Value = $"{x.AuthorId}",
                     Text = $"{x.FirstName} {x.LastName}"
-                });
-            ViewBag.Authors = authors;
-            return View();
+                });*/
+           // ViewBag.Authors = authors;
+            var vm = new PostCreateVM();
+            GetAuthorNames(vm);
+            return View(vm);
         }
  
         [Route("/create")]
         [HttpPost]  
-        public IActionResult Create(Post item)  
+        public IActionResult Create(PostCreateVM vm)  
         {  
-            if (item != null) {
-                var post = new Blogging.Models.Post() {
-                    Title = item.Title,
-                    Content = item.Content,
-                    AuthorId = item.AuthorId,
-                    Date = DateTime.Now,
-                };    
+            var post = vm.Post;
+            var author = GetAuthor(vm);
+            if (ModelState.IsValid) {
+                post.Date = DateTime.Now;
+                post.AuthorName = author;
+                post.Content = vm.Post.Content;
+                post.Title = vm.Post.Title;
+                //var post = new Blogging.Models.Post() {
+                  //  Title = item.Title,
+                  //  Content = item.Content,
+                 //   AuthorId = item.AuthorId,
+                   // Date = DateTime.Now,
+               // };  
                 // Add blog post to database
                 _blogContext.Posts.Add(post);
                 _blogContext.SaveChanges();
+                return RedirectToAction("Index");
             }
-            return RedirectToAction("Index");        
+            GetAuthorNames(vm);
+            return View(vm);      
         }
 
         [Route("/edit/{id}")]
@@ -72,12 +83,10 @@ namespace Blogging.Controllers
         {  
             var post = _blogContext.Posts.Find(id); 
             if (ModelState.IsValid) {
-              
                 post.Title = item.Title;
                 post.Content = item.Content;
                 post.Date = DateTime.Now;
                 
-                // Add blog post to database
                 _blogContext.Posts.Update(post);
                 _blogContext.SaveChanges();
                 return RedirectToAction("Index");        
@@ -89,39 +98,34 @@ namespace Blogging.Controllers
         [HttpGet]
         public IActionResult Details([FromRoute]int id)
         {
-            var authors = _blogContext.Authors
-                .Select(x => new SelectListItem {
-                    Value = x.AuthorId.ToString(),
-                    Text = $"{x.FirstName} {x.LastName}"
-                });
-            ViewBag.Authors = authors;
-            var authorList = new List<SelectListItem>();
             var post = _blogContext.Posts.Find(id);
-            var comments = _blogContext.Comments.Where(c => c.PostId == post.PostId).ToList();
-            var vm = new Models.PostCreateVM(){
-                Post = post,
+            var author = _blogContext.Authors.Find(post.AuthorId);
+            var comments = _blogContext.Comments.Where(x => x.PostId == post.PostId).ToList();
+            var vm = new PostCreateVM(){
+                Post = post, 
                 Comments = comments,
-                AuthorList = authorList,
             };
+            GetAuthorNames(vm);
             return View(vm);
         }
 
         [Route("/show/{id}/comment")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Comment([FromRoute] int id, Comment item) {
-            var comment = new Blogging.Models.Comment(){
-                Content = item.Content,
-                AuthorId = item.AuthorId,
-                PostId = id
-            };    
-            // Add blog post to database
-            _blogContext.Comments.Add(comment);
-            _blogContext.SaveChanges();
-        
-            return View("Details", item);
+        public IActionResult Comment([FromRoute] int id, PostCreateVM vm) {
+            var comment = vm.Comment;
+            var author = GetAuthor(vm);
+            if (ModelState.IsValid) {
+                comment.Content = vm.Comment.Content;
+                comment.PostId = id;
+                comment.Author = author;
+                _blogContext.Comments.Add(comment);
+                _blogContext.SaveChanges();
+                return RedirectToAction("Details"); 
+            }
+            GetAuthorNames(vm);
+            return View();
         }
-
 
         [Route("/delete/{id}")]
         [HttpPost]  
@@ -133,6 +137,22 @@ namespace Blogging.Controllers
             return RedirectToAction("Index");  
         }  
 
+        private void GetAuthorNames(PostCreateVM vm)
+        {
+        vm.AuthorList = _blogContext.Authors
+            .Select(x => new SelectListItem {
+                Value = x.AuthorId.ToString(),
+                Text = $"{x.FirstName} {x.LastName}"
+            })
+            .ToList();
+        }
+
+        private Author GetAuthor(PostCreateVM vm)
+        {
+            int.TryParse(vm.SelectedAuthorId, out var AuthorId);
+            var author = _blogContext.Authors.Find(AuthorId);
+            return author;
+        }
     
     }
 }
